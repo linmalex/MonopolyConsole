c185526f3792bd51baee8ef619869ce2787a1068
Lindsay Alexander <linmalex@gmail.com>
2018-11-04 19:43:06 -0800

    add PlayCard(Card) to Player.cs to check if card is a money card and if so, add it to bank (more logic to come); create BankTotal(playerBank) in Player.cs to calculate total in player's bank; update TakeTurn() in Player.cs to use PlayCard() and BankTotal();

--- MonopolyConsole/Models/Player.cs
+++ MonopolyConsole/Models/Player.cs
@@ -20,6 +20,7 @@ namespace MonopolyConsole.Models
         public Player()
         {
             CompleteSetCount = 0;
+            Bank = new List<Card>();
         }
 
         //methods
@@ -28,9 +29,6 @@ namespace MonopolyConsole.Models
             hand.Add(deck.Cards[0]);
             deck.Cards.RemoveAt(0);
         }
-        public void DepositToBank(Card card) { }
-        public void AddToBoard(Card card) { }
-        public void PlayToCenter(Card card) { }
 
         public static void TakeTurn(Player player, Deck deck, List<Card> hand)
         {
@@ -42,10 +40,33 @@ namespace MonopolyConsole.Models
             for (int i = 0; i < cardsToPlay; i++)
             {
                 Console.WriteLine("Which card would you like to play? To choose, select the nth card in your hand");
-                var nthCard = Convert.ToInt32(Console.ReadLine());
+                var nthCard = Convert.ToInt32(Console.ReadLine()) - 1;
                 var cardToPlay = hand[nthCard];
-                cardToPlay.Play();
+                player.PlayCard(cardToPlay, player);
+                player.Hand.RemoveAt(nthCard);
+                
             }
+            int bankTotal = Player.BankTotal(player.Bank);
+            Console.WriteLine("You have {0} million dollars in the bank", bankTotal);
+        }
+
+        public void PlayCard(Card card, Player player)
+        {
+            var cardType = card.GetType().Name;
+            if (cardType == "MoneyCard")
+            {
+                player.Bank.Add(card);
+            }
+        }
+
+        public static int BankTotal(List<Card> hand)
+        {
+            int bankTotal = 0;
+            foreach (Card card in hand)
+            {
+                bankTotal += card.Value;
+            }
+            return bankTotal;
         }
     }
 }
